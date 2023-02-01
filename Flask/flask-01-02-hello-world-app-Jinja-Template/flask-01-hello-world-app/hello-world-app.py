eb0fbcaae4c831eef35730ad2bfc1b96dbfe9530
huseyingultekin <huseyingultekin506@gmail.com>
2023-02-01 00:35:33 +0000

    okey

---
+++ Flask/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,22 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello world from Flask!"
+
+@app.route('/second')
+def second():
+    return "Merhabalar"
+
+@app.route('/third/subthird')
+def third():
+    return "This is the subpage of third page"
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f"Id number of this page {id} "
+
+if __name__ == "__main__":
+    app.run(debug=True)
